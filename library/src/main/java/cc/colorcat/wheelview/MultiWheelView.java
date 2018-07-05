7e1c6d064b3b38c00edaf18978a5ed5e3bb3800a
cxx <xx.ch@outlook.com>
2018-07-05 23:08:13 +0800

    Initial commit

---
+++ library/src/main/java/cc/colorcat/wheelview/MultiWheelView.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2018 cxx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.colorcat.wheelview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by cxx on 2018/4/28.
+ * xx.ch@outlook.com
+ */
+public class MultiWheelView extends LinearLayout {
+    private int mCount;
+    private WheelView[] mViews;
+    private Object[] mData;
+    private OnSelectedChangeListener mListener;
+
+    public MultiWheelView(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public MultiWheelView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public MultiWheelView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public MultiWheelView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        super.setOrientation(LinearLayout.HORIZONTAL);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MultiWheelView);
+        @LayoutRes
+        int layout = ta.getResourceId(R.styleable.MultiWheelView_wheelViewLayout, R.layout.widget_wheel_view);
+        mCount = ta.getInteger(R.styleable.MultiWheelView_wheelViewCount, 1);
+        ta.recycle();
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        mData = new Object[mCount];
+        mViews = new WheelView[mCount];
+        for (int i = 0; i < mCount; ++i) {
+            WheelView view = (WheelView) inflater.inflate(layout, this, false);
+            view.setOnItemSelectedListener(i != mCount - 1 ? new WheelViewSelectedListener(i) : new LastWheelViewSelectedListener());
+            view.setViewBinder(new MultiViewBinder(i));
+            addView(view);
+            mViews[i] = view;
+            mData[i] = new ArrayList<Node>();
+        }
+    }
+
+    public void setOnSelectedChangeListener(OnSelectedChangeListener listener) {
+        mListener = listener;
+    }
+
+    public OnSelectedChangeListener getOnSelectedChangeListener() {
+        return mListener;
+    }
+
+    public int[] getSelectedPositions() {
+        int[] result = new int[mCount];
+        for (int i = 0; i < mCount; ++i) {
+            result[i] = mViews[i].getSelectedItemPosition();
+        }
+        return result;
+    }
+
+    public void setData(List<? extends Node> data) {
+        List<Node> first = getData(0);
+        first.clear();
+        first.addAll(data);
+        mViews[0].setItemData(first);
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Node> getData(int index) {
+        return (List<Node>) mData[index];
+    }
+
+
+    private class MultiViewBinder implements WheelView.ViewBinder {
+        private final int mIndex;
+
+        private MultiViewBinder(int index) {
+            mIndex = index;
+        }
+
+        @Override
+        public boolean onBind(WheelView.ItemViewHolder holder, int position) {
+            holder.textView.setText(getData(mIndex).get(position).contentToString());
+            return true;
+        }
+    }
+
+
+    private class WheelViewSelectedListener implements WheelView.OnItemSelectedListener {
+        private final int mIndex;
+
+        private WheelViewSelectedListener(int index) {
+            mIndex = index;
+        }
+
+        @Override
+        public void onItemSelected(int position) {
+            List<Node> next = getData(mIndex + 1);
+            next.clear();
+            next.addAll(position != WheelView.INVALID_POSITION ? getData(mIndex).get(position).children() : Collections.<Node>emptyList());
+            mViews[mIndex + 1].setItemData(next);
+        }
+    }
+
+
+    private class LastWheelViewSelectedListener implements WheelView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(int position) {
+            if (mListener != null) {
+                int[] selectedPositions = new int[mCount];
+                for (int i = 0; i < mCount; ++i) {
+                    selectedPositions[i] = mViews[i].getSelectedItemPosition();
+                }
+                mListener.onSelectedChanged(selectedPositions);
+            }
+        }
+    }
+
+
+    public interface OnSelectedChangeListener {
+        void onSelectedChanged(int... positions);
+    }
+
+
+    public interface Node {
+        String contentToString();
+
+        List<? extends Node> children();
+    }
+}
---
+++ library/src/main/java/cc/colorcat/wheelview/WheelView.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright 2018 cxx
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.colorcat.wheelview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by cxx on 2018/4/26.
+ * xx.ch@outlook.com
+ */
+public class WheelView extends FrameLayout {
+    public static final int INVALID_POSITION = -1;
+
+    private View mCoverView;
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mManager;
+    private RecyclerView.Adapter mAdapter;
+    private List<Object> mData = new ArrayList<>();
+    @LayoutRes
+    private int mItemLayout;
+    private int mDisplayCount; // 同时显示的 item 数量
+    private int mPlaceholderCount; // 占位的 item 数量，实际 item 数量 = 用户设置的数据的数量 + 2 * mPlaceholderCount
+    private int mItemHeight = Integer.MIN_VALUE;
+    private int mSelectedPosition = WheelView.INVALID_POSITION; // 正中间的 item 的 position
+    private OnItemSelectedListener mListener;
+    private ViewBinder mBinder;
+
+    public WheelView(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public WheelView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public WheelView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public WheelView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.WheelView);
+        mDisplayCount = ta.getInteger(R.styleable.WheelView_displayCount, 3);
+        if ((mDisplayCount & 1) == 0) {
+            if (mDisplayCount > 5) {
+                --mDisplayCount;
+            } else {
+                ++mDisplayCount;
+            }
+        }
+        mPlaceholderCount = mDisplayCount >> 1;
+        mItemLayout = ta.getResourceId(R.styleable.WheelView_itemLayout, android.R.layout.simple_list_item_1);
+        int coverColor = ta.getColor(R.styleable.WheelView_coverColor, Color.TRANSPARENT);
+        ta.recycle();
+
+        mRecyclerView = new RecyclerView(context);
+        mRecyclerView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        mManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(mManager);
+        mAdapter = new WheelViewAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    int first = mManager.findFirstCompletelyVisibleItemPosition();
+                    if (mSelectedPosition != first) {
+                        mSelectedPosition = first;
+                        notifyItemSelectedChanged();
+                    }
+                }
+            }
+        });
+        SnapHelper helper = new LinearSnapHelper();
+        helper.attachToRecyclerView(mRecyclerView);
+        addView(mRecyclerView);
+
+        if (coverColor != Color.TRANSPARENT) {
+            createCoverView(context);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                mCoverView.setBackground(buildCoverBackground(coverColor));
+            } else {
+                mCoverView.setBackgroundDrawable(buildCoverBackground(coverColor));
+            }
+        }
+    }
+
+    private Drawable buildCoverBackground(@ColorInt int coverColor) {
+        int red = Color.red(coverColor);
+        int green = Color.green(coverColor);
+        int blue = Color.blue(coverColor);
+        int alpha = Color.alpha(coverColor);
+
+        int quarter = Color.argb((int) (alpha * 0.75), red, green, blue);
+        int center = Color.argb((int) (alpha * 0.1), red, green, blue);
+        int[] colors = {coverColor, quarter, center, quarter, coverColor};
+        return new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
+    }
+
+    public void setCoverBackground(Drawable drawable) {
+        if (mCoverView == null) {
+            createCoverView(getContext());
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mCoverView.setBackground(drawable);
+        } else {
+            mCoverView.setBackgroundDrawable(drawable);
+        }
+    }
+
+    private void createCoverView(Context context) {
+        mCoverView = new View(context);
+        mCoverView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        addView(mCoverView);
+    }
+
+    public void setItemData(List<?> data) {
+        if (data == null) throw new IllegalArgumentException("data == null");
+
+        mData.clear();
+        pushPlaceholderData();
+        mData.addAll(data);
+        pushPlaceholderData();
+        mAdapter.notifyDataSetChanged();
+        if (data.isEmpty()) {
+            mSelectedPosition = WheelView.INVALID_POSITION;
+        } else {
+            mSelectedPosition = 0;
+            mRecyclerView.smoothScrollToPosition(mSelectedPosition);
+        }
+        notifyItemSelectedChanged();
+    }
+
+    private void pushPlaceholderData() {
+        for (int i = 0; i < mPlaceholderCount; ++i) {
+            mData.add("");
+        }
+    }
+
+    private void notifyItemSelectedChanged() {
+        if (mListener != null) {
+            mListener.onItemSelected(mSelectedPosition);
+        }
+    }
+
+    public void setViewBinder(ViewBinder binder) {
+        mBinder = binder;
+    }
+
+    public void setOnItemSelectedListener(OnItemSelectedListener listener) {
+        mListener = listener;
+    }
+
+    public int getSelectedItemPosition() {
+        return mSelectedPosition;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (changed) {
+            mItemHeight = (bottom - top) / mDisplayCount;
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+
+    private class WheelViewAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+
+        @NonNull
+        @Override
+        public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(mItemLayout, parent, false);
+            return new ItemViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+            if (mBinder == null
+                    || position < mPlaceholderCount
+                    || position >= mData.size() - mPlaceholderCount
+                    || !mBinder.onBind(holder, position - mPlaceholderCount)) {
+                holder.textView.setText(String.valueOf(mData.get(position)));
+            }
+            ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+            if (mItemHeight != Integer.MIN_VALUE && lp.height != mItemHeight) {
+                lp.height = mItemHeight;
+                holder.itemView.setLayoutParams(lp);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+    }
+
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        public final TextView textView;
+
+        private ItemViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(android.R.id.text1);
+        }
+    }
+
+    private static void setBackground(View view, Drawable background) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackground(background);
+        } else {
+            view.setBackgroundDrawable(background);
+        }
+    }
+
+
+    public interface OnItemSelectedListener {
+        void onItemSelected(int position);
+    }
+
+
+    public interface ViewBinder {
+        /**
+         * @return 如果手动处理数据绑定则返回 true，否则返回 false
+         */
+        boolean onBind(ItemViewHolder holder, int position);
+    }
+}
